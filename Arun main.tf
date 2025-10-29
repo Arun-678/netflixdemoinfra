f32b5efef45b9d60e8bdf9a881532803dffa5bd6
Arun-678 <arun.aws678@gmail.com>
2025-10-29 15:36:02 -0700

    Update Arun main.tf

--- Arun main.tf
+++ Arun main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-04aa00acb1165b32a"
+ami = "ami-0360c520857e3138f"
 instance_type = "t2.medium"
 key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+vpc_security_group_ids = ["sg-043b2f942672032aa"]
 tags = {
 Name = var.instance_names[count.index]
 }
